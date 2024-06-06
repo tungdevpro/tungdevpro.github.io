fe69942b53bd4f4a80d988a97748107b6c102f29
tungdevpro <tungdo21899@gmail.com>
2024-06-06 22:05:57 +0700

    hotfix: install vercel & setup adapter

--- astro.config.ts
+++ astro.config.ts
@@ -5,10 +5,14 @@ import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
+import vercel from '@astrojs/vercel/serverless';
+
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
+  output: 'server',
+  adapter: vercel(),
   integrations: [
     tailwind({
       applyBaseStyles: false,
